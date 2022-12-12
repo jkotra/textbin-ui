9f55bbd2ead8c6228b8cca3eef8321fb2e2edf55
Jagadeesh Kotra <jagadeesh@stdin.top>
2022-12-12 13:44:00 +0530

    UX changes
    - fix favicon
    - handle network errors in home.
    - add padding (p-2)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,7 +14,7 @@ export class HomeComponent {
 
   form: UntypedFormGroup = new UntypedFormGroup({});
   submit_btn_classes = { 'btn': true, 'btn-primary': true, 'mt-2': true, 'disabled': false };
-  error = { error: false, message: "" }
+  error = { error: false, message: "Error" }
   grc_key = "6LeWS8ciAAAAAGjDOhR7rMpcXLfITm6i8a81gV2Y";
 
   constructor(private ps: PasteService, private fb: UntypedFormBuilder, private router: Router) {
@@ -34,6 +35,7 @@ export class HomeComponent {
   }
 
   savePaste(): void {
+    /* this.form.get('captcha')?.setValue('SomethingInvalid'); */
     if (this.form.valid) {
       this.submit_btn_classes.disabled = true
       this.ps.post(this.form.value).subscribe(
@@ -42,7 +44,12 @@ export class HomeComponent {
             this.router.navigateByUrl(`/uuid/${v.uuid}`, { state: v })
           },
           error: (err: any) => {
-            console.error(err);
+            if (err instanceof HttpErrorResponse) {
+              this.error.error = true;
+              this.error.message = `Error: ${err.message}`;
+              this.submit_btn_classes.disabled = false;
+              return;
+            }
             this.error.error = true;
             this.error.message = err.error.message;
             this.submit_btn_classes.disabled = false;
